f16783c32ae60a77156025fcc62c32dfb0fc76fc
Kenneth Vidal <130331310+KnthV@users.noreply.github.com>
2025-10-07 18:47:52 +0800

    Update Posts.js

--- client/src/pages/Posts.js
+++ client/src/pages/Posts.js
@@ -8,12 +8,12 @@ const PostList = () => {
     <div>
       
       {/* Show all posts */}
-      {/* <PostCard postType="all" maxPosts={20} /> */}
+      <PostCard postType="all" maxPosts={20} />
       
       {/* Or filter by specific post type */}
       {/* <PostCard postType="Waste Post" maxPosts={10} /> */}
       {/* <PostCard postType="Initiative Post" maxPosts={10} /> */}
-      <PostCard postType="Forum Post" maxPosts={10} />
+      {/* <PostCard postType="Forum Post" maxPosts={10} /> */}
     </div>
   );
 };
